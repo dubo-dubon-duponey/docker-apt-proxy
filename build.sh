d615395b23afae1afd95c44ecb101d9554fc4fdc
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-01-18 22:48:44 -0800

    Bang
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit -o errtrace -o functrace -o nounset -o pipefail
+
+export TITLE="OMBI"
+export DESCRIPTION="A dubo image for OMBI"
+export IMAGE_NAME="ombi"
+
+# shellcheck source=/dev/null
+. "$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/helpers.sh"
