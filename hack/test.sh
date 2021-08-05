37516b3d9ea28bff176c7eaa44e531992f2c9aed
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-08-05 15:59:44 -0700

    CI
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- hack/test.sh
+++ hack/test.sh
@@ -4,6 +4,9 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 # shellcheck source=/dev/null
 root="$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/../"
 
+readonly name="${1:-image}"
+shift || true
+
 # Simple no-thrill build tester
 if ! "$root/hack/build.sh" \
     --inject registry="ghcr.io/dubo-dubon-duponey" \
@@ -11,7 +14,7 @@ if ! "$root/hack/build.sh" \
 	  --inject date=2021-08-01 \
 	  --inject suite=bullseye \
     --inject platforms=linux/amd64,linux/arm64 \
-  	"$@"; then
+  	"$name" "$@"; then
   printf >&2 "Failed building\n"
   exit 1
 fi
